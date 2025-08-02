cb497357b4c38e8f4f405731e99c7ce6550bb72b
Pablo Basso <pablobasso25@gmail.com>
2025-08-02 00:30:26 -0300

    ejercicios de javascript

---
+++ TP-1/resultadosTP1.js
@@ -0,0 +1,43 @@
+
+/* 1
+alert ("Un mensaje"); */
+
+/* 2
+document.writeln("«Hello World»"); */
+
+/* 3
+document.writeln(`${3+5}`);*/
+
+/* 4
+let usuario = prompt("¿Cuál es tu nombre?");
+alert (`Hola ${usuario}`); */
+
+/* 5
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
+alert(`El resultado es ${num1 + num2}`); */
+
+/* 6
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
+
+if (num1 > num2) {
+    document.writeln(`El ${num1} es mayor`);
+}else {
+    document.writeln(`El ${num2} es mayor`);
+} */
+
+/* 7
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
+let num3 = parseInt(prompt("Ingrese el tercer número"));
+
+if (num1 > num2 && num1 > num3){
+    document.writeln(`El ${num1} es mayor`);
+
+} else if (num2 > num1 && num2 > num3) {
+    document.writeln(`El ${num2} es mayor`);
+
+}else {
+    document.writeln(`El ${num3} es mayor`);
+} */
