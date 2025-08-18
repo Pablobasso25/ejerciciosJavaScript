d3a21f380263bf92f319a75ef37135262567ba6b
Pablo Basso <pablobasso25@gmail.com>
2025-08-17 22:52:46 -0300

    Add challenge-13 folder

--- TP-2/resultadosTP2.js
+++ TP-2/resultadosTP2.js
@@ -80,7 +80,7 @@ alert("Cadenas concatenadas: " + texto); */
 
 
 
-let letrasDNI = [
+/* let letrasDNI = [
   "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X",
   "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"
 ];
@@ -106,4 +106,97 @@ do {
     alert(`DNI: ${dni} → Letra: '${letra}'`);
   }
 
-} while (true);
+} while (true); */
+
+do{
+    let entrada = parseInt(prompt("Ingrese el dni:"));
+
+    
+    if(isNaN(entrada) || entrada<0 || entrada>99999999){
+        alert("El valor ingresado esta fuera de los limites o esta vacio");
+    }
+    else{
+      let resto = entrada % 23;
+      let letra = "";
+     /*Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E) */ 
+      switch(resto)
+      {
+        case 0: letra = "T";
+        break;
+        
+        case 1:letra = "R";
+        break;
+
+        case 2:letra = "W";
+        break;
+
+        case 3:letra = "A";
+        break;
+
+        case 4:letra = "G";
+        break;
+
+        case 5:letra = "M";
+        break;
+
+        case 6:letra = "Y";
+        break;
+
+        case 7:letra = "F";
+        break;
+
+        case 8:letra = "P";
+        break;
+
+        case 9:letra = "D";
+        break;
+
+         /*Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E) */ 
+
+        case 10:letra = "X";
+        break;
+
+        case 11:letra = "B";
+        break;
+
+        case 12:letra = "N";
+        break;
+
+        case 13:letra = "J";
+        break;
+
+        case 14:letra = "Z";
+        break;
+
+        case 15:letra = "S";
+        break;
+
+        case 16:letra = "Q";
+        break;
+
+        case 17:letra = "V";
+        break;
+
+        case 18:letra = "H";
+        break;
+
+        case 19:letra = "L";
+        break;
+
+        case 20:letra = "C";
+        break;
+
+        case 21:letra = "K";
+        break;
+
+        case 22:letra = "E";
+        break;
+      }
+      alert("El dni es " + entrada + " La letra es " + letra);
+    }
+
+    let respuesta=confirm("Precione cancelar para terminar el programa?");
+    if(!respuesta){
+        break;
+    }
+}while(true);
