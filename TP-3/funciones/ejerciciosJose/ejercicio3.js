fc6dfcf14235c1659acb5bb433f5b5dd9327a7f2
Pablo Basso <pablobasso25@gmail.com>
2025-08-22 00:24:22 -0300

    add exercises

--- TP-3/funciones/ejerciciosJose/ejercicio3.js
+++ TP-3/funciones/ejerciciosJose/ejercicio3.js
@@ -1,2 +1,14 @@
 
 // 3. Crea una función que reciba un string y devuelva el número de volcales que contiene
+
+
+let frase = prompt("Escribí una frase:");
+let vocales = "";
+
+for (let i = 0; i < frase.length; i++) {
+    let letra = frase.charAt(i);
+    if ("aeiouAEIOU".includes(letra)){
+        vocales += letra;
+        alert(`Las vocales que aparecen son: ${vocales}`);
+    }
+} 
