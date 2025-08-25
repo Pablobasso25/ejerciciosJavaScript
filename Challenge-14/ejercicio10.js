1fe476e247312ef46737e7684627b4024f4d6f7e
Pablo Basso <pablobasso25@gmail.com>
2025-08-25 17:05:07 -0300

    add exercises

--- Challenge-14/ejercicio10.js
+++ Challenge-14/ejercicio10.js
@@ -1,3 +1,22 @@
 
 /* Mostrar la primera letra de un texto: 
 Crea una función que reciba un texto y devuelva su primera letra con .charAt(). */
+
+function primeraLetra(texto) {
+  if (                                ///verifica que no esté vacio, que tenga al menos 3 letras, no contenga símbolos ni números
+  texto &&
+  texto.trim() !== "" &&
+  texto.length >= 3 &&
+  /^[a-zA-Z]+$/.test(texto)
+)
+
+ {
+    return texto.charAt(0);
+  } else {
+    return "Entrada inválida";
+  }
+}
+
+let palabra = prompt("Ingresá una palabra");
+let letra = primeraLetra(palabra);
+alert("La primera letra es: " + letra);
--- Challenge-14/ejercicio9.js
+++ Challenge-14/ejercicio9.js
@@ -1,3 +1,10 @@
 
 /* Generar un número aleatorio entre 1 y 10: 
 Crea una función que devuelva un número aleatorio entre 1 y 10 usando Math.random() y Math.floor(). */
+
+function numeroAleatorio() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+let aleatorio = numeroAleatorio();
+alert("Tu número aleatorio entre 1 y 10 es: " + aleatorio);
