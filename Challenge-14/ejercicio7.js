591f8736d78c83b827cea72ce7c7e123a7c6e006
Pablo Basso <pablobasso25@gmail.com>
2025-08-25 10:20:07 -0300

    add exercises

--- Challenge-14/ejercicio7.js
+++ Challenge-14/ejercicio7.js
@@ -1,3 +1,49 @@
 
 /* Calculadora básica con switch: 
 Crea una función que reciba dos números y un operador (+, -, *, /). Devuelva el resultado de la operación. Si el operador no es válido, mostrar error. */
+
+function calculo (num1, num2, operador){
+    let resultado;
+    switch (operador) {
+        case "+":
+            resultado = num1 + num2;
+        break;
+        case "-":
+            resultado = num1 - num2;
+        break;
+        case "*":
+            resultado = num1 * num2;
+        break;
+        case "/":
+            if (num2 !== 0) {
+            resultado = num1 / num2;
+            } else {
+            resultado = "No se puede dividir por cero";
+            }
+        break;            
+        default:
+            resultado = "Operador inválido";
+        break;
+    }
+    return resultado;
+}
+
+let resultado;
+
+do{
+let num1 = parseInt(prompt("Ingresa el primer número"));
+let num2 = parseInt(prompt("Ingresa el segundo número"));
+let operador = prompt("Qué operación vas a realizar? (+, -, *, /)");
+
+    if (isNaN(num1) || isNaN(num2)) {          // si no son números
+        alert("Por favor, ingresá números válidos.");
+    } else if (!["+", "-", "*", "/"].includes(operador)) {
+    alert("Operador inválido. Por favor ingresá: +, -, *, o /");
+    }else {
+        resultado = calculo(num1, num2, operador)
+        alert ("El resultado es: " + resultado)
+        /* alert("El resultado es: " + calculo(num1, num2, operador)); */
+        break;
+    }
+    
+}while (true);
