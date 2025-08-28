fa578b9f1d5198d0f7571bab7a859d8ed52edaa1
Pablo Basso <pablobasso25@gmail.com>
2025-08-27 21:36:51 -0300

    add objects folder

---
+++ Objeto/ejercicio1.js
@@ -0,0 +1,23 @@
+
+
+// crea un objeto con 3 propiedades
+const persona = {
+    nombre: "José",
+    edad: 29,
+    profesion: "Ingeniero",
+
+};
+
+// Accede y muestra su valor
+console.log(persona);
+
+// Agrega una nueva propiedad
+persona.altura = 175
+console.log(persona);
+
+// Elimina una de las 3 primeras propiedades
+delete persona.edad
+console.log(persona);
+
+// Agrega una función e involucrala
+
