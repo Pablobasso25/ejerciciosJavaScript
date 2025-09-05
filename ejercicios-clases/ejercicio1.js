0227fd62a21cc680a2b65474c748ba9dbaea98b0
Pablo Basso <pablobasso25@gmail.com>
2025-09-05 17:21:48 -0300

    add exercises

--- ejercicios-clases/ejercicio1.js
+++ ejercicios-clases/ejercicio1.js
@@ -1,13 +1,4 @@
 
-
-
-
-// Modifica la clase con get y set para que use propiedades privadas
-// Utiliza los get y set y muestra sus valores
-// Sobreescribe un metodo de una clase que utilice herencia
-
-
-
 // Crea un clase que reciba dos propiedades
 class Animal {
     constructor (especieParam,edadParam){
@@ -31,13 +22,63 @@ const gato = new NuevoAnimal ("gato",3,"blanco");
 console.log(gato);
 
 
-// Crea una clase que haga uso de get y set
-class Persona {
-    #profesion
 
-    constructor (_nombre, edad,profesion){
+// Crea una clase que haga uso de get y set
+class GetSetPersona {
+    #profesion
+    #dni
+    constructor (_nombre, edad, profesion, dni){
         this._nombre = _nombre;
         this.edad = edad;
-        this.#profesion = profesion
+        this.#profesion = profesion;
+        this.#dni = dni
     }
+
+    presentarse () {
+        return `Hola, soy ${this._nombre} y tengo ${this.edad} años.`
+    }
+
+
+
+    get profesion (){
+        return this.#profesion
+    }
+
+    
+    /* get dni(){            // descomentar para ver el nuevo valor !!
+        return this.#dni
+    } */
+
+    set dni(NuevoDni){
+        this.#dni = NuevoDni;
+    }
+
 }
+
+// Utiliza los get y set y muestra sus valores
+const persona = new GetSetPersona ("Juan", 32, "abogado", "40902304");
+console.log(persona);
+console.log(persona.presentarse());
+console.log(persona.profesion);
+
+persona.dni = "36700222";
+console.log(persona.dni);
+
+
+
+
+// Sobreescribe un metodo de una clase que utilice herencia
+class NuevaPersona extends GetSetPersona{
+    constructor (_nombre, edad, cargo){
+        super(_nombre,edad);
+        this.cargo = cargo;
+    }
+
+    presentarse () {
+    return `Hola, soy ${this._nombre} y tengo ${this.edad} años y trabajo de ${this.cargo}`
+    }
+
+}
+const empleado = new NuevaPersona ("Pablo", 32, "Desarrollador");
+console.log(empleado.presentarse());
+
