8651dae046ef1e69edb1a21b7a49f57f97068d49
Pablo Basso <pablobasso25@gmail.com>
2025-09-11 21:15:06 -0300

    add theory

--- ejercicios-combinados/ejercicio1.js
+++ ejercicios-combinados/ejercicio1.js
@@ -3,7 +3,7 @@
 
     class Cuenta {
         constructor (titular, saldoInicial = 0){
-            this.titular = titular;
+            this.nombre = titular;
             this.saldo = saldoInicial
         }
 
@@ -26,5 +26,21 @@
 
         informacionCuenta (){
             return `Hola ${this.titular}, tu estado actual de cuenta es: $${this.saldo}`;
+        }
+    }
+
+    // array contenedor de cuentas 
+    let cuentas = [];
+    let crearOtra;
+
+    do{
+        // pido datos para crear la cuenta 
+        let nombre = prompt("Ingresá tu mombre de usuario para crear tu cuenta");
+        let saldoInicial = parseFloat(prompt("Ingresá el saldo inicial"));
+
+        // validaciones de datos
+        if (nombre && !isNaN(saldoInicial) && saldoInicial >= 0) { // si los datos son válidos (ingresa un nombre, el saldo es un número y el saldo es mayor a 0), se contunua 
+            let nuevaCuenta = new cuenta(titular, saldoInicial);  // creo una nueva instancia que guarda los datos de la cuenta
+            cuentas.push(nuevaCuenta);
         }
     }
