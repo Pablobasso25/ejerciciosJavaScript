fce4b83ba65d6d1d5919efbb6ed36d7bed993ecf
Pablo Basso <pablobasso25@gmail.com>
2025-09-12 21:42:21 -0300

    add exercises

--- TP-4/ejercicio2.js
+++ TP-4/ejercicio2.js
@@ -10,138 +10,93 @@ Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y vo
  */
 
 
-/* const cuenta = {
+ const cuenta = {
     titular: "Alex",
     saldo: 0,
-
-    ingresar: function(cantidad) {
-        if (cantidad > 0) {
-            return this.saldo += cantidad
+    ingresar: function(cantidad){
+        if (cantidad > 0){
+            return this.saldo += cantidad;
         } else {
-            return alert ("Ingresá un valor mayor a 0");
-        }
+            return alert("Ingresá un valor mayor a 0");
+            
+        };
     },
-
-
     extraer: function(cantidad){
-        if (cantidad > this.saldo) {
-            return alert ("Saldo insuficiente. Por favor, ingresá un monto menor o igual a:" + this.saldo)
-        }else {
-            return this.saldo -= cantidad
+        if (cantidad > this.saldo){
+            return alert("Saldo insuficiente. Por favor, ingresá un monto menor o igual a:" + this.saldo)
+        } else {
+            return this.saldo -= cantidad;
         }
     },
 
-    informacionCuenta: function(){
-
-        return `Hola ${this.titular}, tu estado actual de cuenta es: $${this.saldo}`
+    informacionCuenta: function (){
+        return `Hola ${this.titular}, tu estado actual de cuenta es: ${this.saldo}`
     },
-    }
+}
 
-    let entrada;
-
-    do{
-        entrada = prompt("Qué querés hacer? (ingresar / extraer / ver / salir)").toLowerCase()
-        if (entrada == "ingresar"){
-            let monto = parseFloat(prompt("Qué monto vas a ingresar"));
-            if (isNaN(monto) || monto <= 0){  //  no es número o si el monto es menor a 0
-                alert ("Monto invalido");
-            }else{
-                cuenta.ingresar(monto);
-                alert (cuenta.informacionCuenta())
-            }
-        }
-
-        if (entrada == "extraer") {
-            let extraccion = parseFloat(prompt("Qué monto vas a extraer"));
-            if (isNaN(extraccion) || extraccion > cuenta.saldo){
-                alert ("Saldo insuficiente. Por favor, ingresá un monto menor o igual a: " + cuenta.saldo)
-            } else {
-                cuenta.extraer(extraccion);
-                alert (cuenta.informacionCuenta())
-            }
-        }
-        if (entrada == "ver") {
+let entrada;
+do{
+    entrada = prompt("¿Qué querés hacer? (ingresar / extraer / ver / salir)").toLowerCase();
+    if (entrada == "ingresar") {
+        let monto = parseInt(prompt("¿Que monto vas a ingresar?"));
+        if(isNaN(monto) || monto <= 0){
+            alert ("Monto inválido");
+        } else {
+            cuenta.ingresar(monto);
             alert (cuenta.informacionCuenta())
         }
-    } while (entrada !== "salir") */
-
-
-    const cuenta = {
-
-    titular : "alex",
-    saldo: 0,
-
-    ingresar: function(){
-        let cantidad= parseFloat(prompt("Ingrese la cantidad a añadir a la cuenta:"));
-
-        if(isNaN(cantidad)){
-            alert("Por favor, ingresa un número válido");
-            return;
-        }
-
-        if(cantidad>0){
-           this.saldo+=cantidad;
-           console.log(`La cantidad ingresada es de : ${cantidad} el saldo total de la cuenta es: ${this.saldo}`);
-        } 
-        else {
-            alert("La cantidad a ingresar debe ser mayor a 0");
-        }
-    }, 
-
-    extraer: function(){
-        let cantidad= parseFloat(prompt("Ingrese la cantidad a extraer de la cuenta:"));
-
-        if(isNaN(cantidad)){
-            alert("Por favor, ingresa un número válido");
-            return;
-        }
-
-        if(cantidad>0){
-           this.saldo-=cantidad;
-           console.log(`La cantidad extraida es : ${cantidad} el saldo total de la cuenta es: ${this.saldo}`);
-        } 
-        else {
-            alert("La cantidad a ingresar debe ser mayor a 0");
-        }
-    },
-    
-    
-    informar : function(){
-        console.log(`El titular : ${this.titular} tiene en su cuenta ${this.saldo}`);
-    },
-
-    menu : function(){
-        let opcion;
-        do{
-           opcion = prompt(
-                `Bienvenido ${this.titular}\nSaldo actual: $${this.saldo}\n` +
-                "1. Ingresar dinero\n" +
-                "2. Extraer dinero\n" +
-                "3. Ver información de la cuenta\n" +
-                "4. Salir\n\n" +
-                "Selecciona una opción:"
-            );
-
-            switch(opcion){
-                case "1":
-                    this.ingresar();
-                    break;
-                case "2":
-                    this.extraer();
-                    break;
-                case "3":
-                    this.informar();
-                    break;
-                case "4":
-                    console.log("Saliendo del programa");
-                    break;
-                default:
-                    console.log("Opcion invalida");
-                    break;
-            }
-        }while(opcion!="4" && opcion !== null);
     }
-};
+    if (entrada == "extraer"){
+        let extraccion = parseInt(prompt("¿Que monto vas a extraer?"));
+         if (isNaN(extraccion) || extraccion > cuenta.saldo){
+            alert("Saldo insuficiente. Por favor, ingresá un monto menor o igual a: " + cuenta.saldo)
+         } else {
+            cuenta.extraer(extraccion);
+            alert (cuenta.informacionCuenta());
+         }
+    }
+    if (entrada == "ver"){
+        alert (cuenta.informacionCuenta())
+    }
+} while (entrada !== "salir")
 
-console.log("Cuenta Bancaria");
-cuenta.menu();
+
+
+    // de este objeto, se crea una clase que me sirve como molde para nuevos usuarios y cuentas
+
+    class Cuenta {
+        constructor (titular, saldoInicial = 0){
+            this.titular = titular,
+            this.saldo = saldoInicial
+        }
+
+        ingresar (cantidad){
+            if (cantidad > 0){
+                this.saldo += cantidad;
+            } else {
+                return "Ingresá un valor mayorr a 0"
+            }
+        }
+
+        extraer (cantidad){
+            if (cantidad > this.saldo){
+                return `Saldo insuficiente. Tenés disponible: $${this.saldo}`;
+            }else {
+                this.saldo -= cantidad;
+                return `Extracción exitosa`
+            }
+        }
+
+        informacionCuenta (){
+            return `Hola ${this.titular}, tu estado actual de cuenta es: $${this.saldo}`;
+        }
+    }
+
+    // creo nuevos objetos (instancias)
+    const cuentaAlex = new Cuenta ("Alex");
+    const cuentaPablo = new Cuenta ("Pablo", 1000)
+
+    console.log(cuentaAlex);
+    console.log(cuentaPablo);
+    console.log(cuentaPablo.extraer(500));
+    console.log(cuentaPablo);
--- ejercicios-combinados/ejercicio1.js
+++ ejercicios-combinados/ejercicio1.js
@@ -25,22 +25,68 @@
         }
 
         informacionCuenta (){
-            return `Hola ${this.titular}, tu estado actual de cuenta es: $${this.saldo}`;
+            return `Hola ${this.nombre}, tu estado actual de cuenta es: $${this.saldo}`;
         }
     }
 
     // array contenedor de cuentas 
-    let cuentas = [];
+    let cuentasCreadas = [];
     let crearOtra;
 
     do{
         // pido datos para crear la cuenta 
-        let nombre = prompt("Ingresá tu mombre de usuario para crear tu cuenta");
+        let nombre = prompt("Ingresá tu nombre de usuario para crear tu cuenta");
         let saldoInicial = parseFloat(prompt("Ingresá el saldo inicial"));
 
         // validaciones de datos
         if (nombre && !isNaN(saldoInicial) && saldoInicial >= 0) { // si los datos son válidos (ingresa un nombre, el saldo es un número y el saldo es mayor a 0), se contunua 
-            let nuevaCuenta = new cuenta(titular, saldoInicial);  // creo una nueva instancia que guarda los datos de la cuenta
-            cuentas.push(nuevaCuenta);
+            let nuevaCuenta = new Cuenta (nombre, saldoInicial);  // creo una nueva instancia que guarda los datos de la cuenta
+            cuentasCreadas.push(nuevaCuenta);    // se guardan los datos de "nuevaCuenta" en el array "cuentas"
+            alert ("Cuenta creada con exito.");
+        }else {
+            alert ("Datos inválidos. Intentá de nuevo.");
+        }
+
+        crearOtra = prompt("¿Querés crear otra cuenta? (si / no)").toLowerCase();
+    } while (crearOtra === "si")   // se repide el bucle mientras el usuario ingrese "si"
+
+
+    //ahora, creo una nueva función fuera del constructor porque no forma parte del comportamiento interno de cada cuenta sino que es aplicada sobre una instancia ya creada  
+    function operarConCuenta (cuenta){
+        let entrada;
+do{
+    entrada = prompt("¿Qué querés hacer? (ingresar / extraer / ver / salir)").toLowerCase();
+    if (entrada == "ingresar") {
+        let monto = parseFloat(prompt("¿Que monto vas a ingresar?"));
+        if(isNaN(monto) || monto <= 0){
+            alert ("Monto inválido");
+        } else {
+            cuenta.ingresar(monto);
+            alert (cuenta.informacionCuenta())
         }
     }
+    if (entrada == "extraer"){
+        let extraccion = parseFloat(prompt("¿Que monto vas a extraer?"));
+         if (isNaN(extraccion) || extraccion > cuenta.saldo){
+            alert("Saldo insuficiente. Por favor, ingresá un monto menor o igual a: " + cuenta.saldo)
+         } else {
+            cuenta.extraer(extraccion);
+            alert (cuenta.informacionCuenta());
+         }
+    }
+    if (entrada == "ver"){
+        alert (cuenta.informacionCuenta())
+    }
+} while (entrada !== "salir")
+
+    }
+    
+
+    let buscarCuenta = prompt("Ingresá tu nombre de usuario para acceder a tu cuenta").toLowerCase()
+    let resultado = cuentasCreadas.find(cuenta => cuenta.nombre.toLowerCase() === buscarCuenta)
+    
+    if (resultado) {
+  operarConCuenta(resultado);
+} else {
+  alert("No se encontró ninguna cuenta con ese nombre.");
+}
