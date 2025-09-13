d3a99b5290bb4588e06ab9fbce8d2f2d814316c7
Pablo Basso <pablobasso25@gmail.com>
2025-09-12 23:34:39 -0300

    add exercises

--- ejercicios-combinados/ejercicio1.js
+++ ejercicios-combinados/ejercicio1.js
@@ -37,6 +37,7 @@ let cuentasCreadas = [];
 let crearOtra;
 
 // Bucle para crear múltiples cuentas
+
 do {
     // pido datos para crear la cuenta
     let nombre = prompt("Ingresá tu nombre de usuario para crear tu cuenta");
@@ -54,13 +55,21 @@ do {
     crearOtra = prompt("¿Querés crear otra cuenta? (si / no)").toLowerCase();
 } while (crearOtra === "si"); // se repide el bucle mientras el usuario ingrese "si"
 
+
+
 //ahora, creo una nueva función fuera del constructor porque no forma parte del comportamiento interno de cada cuenta sino que es aplicada sobre una instancia ya creada
+
 function operarConCuenta(cuenta) {  // el parametro "cuenta" es simbólico, podria llamarse de cualquier otra forma , mas adelante cuando llamo a la función le doy el parametro verdadero
     let entrada;
     do {
         entrada = prompt(
-            "¿Qué querés hacer? (ingresar / extraer / ver / salir)"
-        ).toLowerCase();
+  "¿Qué querés hacer?\n" +
+  "- ingresar\n" +
+  "- extraer\n" +
+  "- ver\n" +
+  "- salir"
+).toLowerCase();
+       
         if (entrada == "ingresar") {
             let monto = parseFloat(prompt("¿Que monto vas a ingresar?"));
             if (isNaN(monto) || monto <= 0) {
@@ -74,9 +83,7 @@ function operarConCuenta(cuenta) {  // el parametro "cuenta" es simbólico, podr
             let extraccion = parseFloat(prompt("¿Que monto vas a extraer?"));
             if (isNaN(extraccion) || extraccion > cuenta.saldo) {
                 alert(
-                    "Saldo insuficiente. Por favor, ingresá un monto menor o igual a: " +
-                    cuenta.saldo
-                );
+                    "Saldo insuficiente. Por favor, ingresá un monto menor o igual a: " + cuenta.saldo);
             } else {
                 cuenta.extraer(extraccion);
                 alert(cuenta.informacionCuenta());
@@ -88,16 +95,20 @@ function operarConCuenta(cuenta) {  // el parametro "cuenta" es simbólico, podr
     } while (entrada !== "salir");
 }
 
+let nombreDeCuentas = cuentasCreadas.map(c => "- " + c.nombre).join("\n");
+alert (`Tus cuentas disponibles son:\n${nombreDeCuentas}`)
+
 // Búsqueda de la cuenta por el nombre
 let resultado;   // se la declara fuera del bucle para poder utilizarla luego cuando llamo a la función
 do{
-    let buscarCuenta = prompt("Ingresá tu nombre de usuario para acceder a tu cuenta").toLowerCase();
+    let buscarCuenta = prompt(`¿A que cuenta querés ingresar?\n${nombreDeCuentas}`).toLowerCase();
     resultado = cuentasCreadas.find((cuenta) => cuenta.nombre.toLowerCase() === buscarCuenta); // el parametro "cuenta" es simbólico, podria llamarse de cualquier otra forma
 
     // Validación de búsqueda y ejecución del menú
-    if (!resultado) {
+    if (!resultado) {    // Si .find(...) no encuentra ninguna coincidencia, devuelve undefined, Entonces "!resultado" se convierte en true, y se ejecuta el bloque del if.
         alert("No se encontró ninguna cuenta con ese nombre. Intentá de nuevo.");
     }
 } while (!resultado)  // si se encuentra una cuenta válida, sale del bucle y contunua
 
     operarConCuenta(resultado); // si se encuentra la cuenta, se pasa la pasa como parametro a la función y se opera con ella
+
