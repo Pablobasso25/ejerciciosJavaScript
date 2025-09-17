26969603de5aa144be0bce10bfdd1ceb8cb45f7c
Pablo Basso <pablobasso25@gmail.com>
2025-09-16 23:13:30 -0300

    add exercises

--- TP-4/ejercicio5.js
+++ TP-4/ejercicio5.js
@@ -12,3 +12,24 @@ mostrarDatos: devuelve toda la información del objeto.
 generaDNI(): genera un número aleatorio de 8 cifras.*/
 
 
+
+class Persona {
+    constructor (nombre, edad, dni, sexo, peso, altura, anioDeNacimiento){
+        this.nombre = nombre,
+        this.edad = edad,
+        this.dni = dni,
+        this.sexo = sexo,
+        this.peso = peso,
+        this.altura = altura,
+        this.anioDeNacimiento = anioDeNacimiento
+    }
+
+    //métodos
+    mostrarGeneracion (){
+        if (this.anioDeNacimiento >= 1994 && this.anioDeNacimiento <= 2010){
+            alert(`${this.nombre} pertenece a la Generación Z\nRasgo característico: Irreverencia`);
+        }
+    }
+}
+const persona1 = new Persona ("pablo", 32, "40902304", "M", 85, 175, 1996);
+persona1.mostrarGeneracion()
