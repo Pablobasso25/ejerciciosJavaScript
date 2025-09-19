0fa90e482357487a48522f4bf4f8dd6f421d8428
Pablo Basso <pablobasso25@gmail.com>
2025-09-18 23:57:25 -0300

    add exercises

--- ejemplo-DOM/ejemplo-2/app.js
+++ ejemplo-DOM/ejemplo-2/app.js
@@ -1,10 +1,18 @@
 
+// creo un objeto
+
 const autor = {
     nombre: "Alvaro Morillo",
+    usuario: "",
+    correo: "",
     descripcion: "Fanático de la tecnología y profesor de las artes de programación con Javascript",
     imagen: "https://img.freepik.com/vector-gratis/ilustracion-joven-sonriente_1308-174669.jpg?semt=ais_incoming&w=740&q=80"
 
 }
+
+// creo una función para crear tarjeta a partir de los datos del objeto
+
+const crearTarjeta = () => {
 
 // creando la etiquetas
 const columnaTarjeta = document.createElement ('div');
@@ -13,6 +21,7 @@ const img = document.createElement ('img');
 const cardBody = document.createElement ('div');
 const cardTitle = document.createElement ('h5');
 const cardText = document.createElement ('p');
+const correo = document.createElement ('p')
 const btn = document.createElement ('a');
 
 // agregando clases y atributos a las etiquetas 
@@ -27,6 +36,8 @@ cardTitle.className = 'card-title';
 cardTitle.textContent = `${autor.nombre}`;
 cardText.className = 'card-text';
 cardText.textContent = `${autor.descripcion}`;
+correo.className = 'text-muted'
+correo.textContent = `${autor.correo}`;
 btn.className = 'btn btn-secondary';
 btn.setAttribute('href', '#');
 btn.textContent = 'Ver perfil';
@@ -40,7 +51,25 @@ card.appendChild(cardBody);
 cardBody.appendChild(cardTitle);
 cardBody.appendChild(cardText);
 cardBody.appendChild(btn);
+}
 
+// función para modificar el autor dependiendo de los datos que se ingresa en los formularios
+
+const modificarAutor = (event) => {
+    event.preventDefault();
+
+    //creo las variables que van a contener lo que se ingrese en los imputs
+    const nombre = document.querySelector('#nombre').value;
+    const usuario = document.querySelector('#usuario').value;
+    const email = document.querySelector('#email').value;
+
+    //llamo al objeto y modifico los valores
+    autor.nombre = nombre;
+    autor.usuario = usuario;
+    autor.email = email;
+}
+
+crearTarjeta()
 
 
 
