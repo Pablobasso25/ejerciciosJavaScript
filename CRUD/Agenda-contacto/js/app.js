5166b4cc235d55a759dac730036a129624713f44
Pablo Basso <pablobasso25@gmail.com>
2025-09-29 19:45:53 -0300

    add exercises

--- CRUD/Agenda-contacto/js/app.js
+++ CRUD/Agenda-contacto/js/app.js
@@ -1,9 +1,14 @@
+import Contacto from "./js/contacto.js";
+
+
+const crearContactoNuevo = new Contacto
+
 
 const btnAgregarContacto = document.getElementById('btnAgregarContacto');
+console.log(btnAgregarContacto);
 
 const modalFormularioContacto = new bootstrap.Modal(document.getElementById('contactoModal'));
 
-btnAgregarContacto.addEventListener('click', ()=> {
-
-    modalFormularioContacto.show()
-} )
+btnAgregarContacto.addEventListener('click', () => {
+    modalFormularioContacto.show();
+})
--- CRUD/Agenda-contacto/js/contacto.js
+++ CRUD/Agenda-contacto/js/contacto.js
@@ -1,32 +1,122 @@
 
 
-class contacto {
+export default class Contacto {
+  #id;
+  #nombre;
+  #apellido;
+  #telefono;
+  #email;
+  #imagen;
+  #empresa;
+  #puestoTrabajo;
+  #direccion;
+  #notas;
 
-    #id;
-    #nombre;
-    #apellido;
-    #telefono;
-    #email;
-    #imagen;
-    #empresa;
-    #puestoTrabajo;
-    #direccion;
-    #notas;
+  constructor(
+    id,
+    nombre,
+    apellido,
+    telefono,
+    email,
+    imagen,
+    empresa,
+    puestoTrabajo,
+    direccion,
+    notas
+  ) {
+    this.#id = crypto.randomUUID();
+    this.#nombre = nombre;
+    this.#apellido = apellido;
+    this.#telefono = telefono;
+    this.#email = email;
+    this.#imagen = imagen;
+    this.#empresa = empresa;
+    this.#puestoTrabajo = puestoTrabajo;
+    this.#direccion = direccion;
+    this.#notas = notas;
+  }
 
+  //getters
 
-    constructor(id, nombre, apellido, telefono, email, imagen, empresa, puestoTrabajo, direccion, notas){
-        this.#id = 0,
-        this.#nombre = nombre,
-        this.#telefono;
-        this.#email;
-        this.#imagen,
-        this.#empresa;
-        this.#puestoTrabajo;
-        this.#direccion;
-        this.#notas;
-    }
+  get id() {
+    return this.#id;
+  }
 
-    //getters
-    get
-    //setters
+  get nombre() {
+    return this.#nombre;
+  }
+
+  get apellido() {
+    return this.#apellido;
+  }
+
+  get telefono() {
+    return this.#telefono;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  get imagen() {
+    return this.#imagen;
+  }
+
+  get empresa() {
+    return this.#empresa;
+  }
+
+  get puestoTrabajo() {
+    return this.#puestoTrabajo;
+  }
+
+  get direccion() {
+    return this.#direccion;
+  }
+
+  get notas() {
+    return this.#notas;
+  }
+
+  //setters
+
+  set id(nuevoId) {
+    this.#id = nuevoId;
+  }
+
+  set nombre(nuevoNombre) {
+    this.#nombre = nuevoNombre;
+  }
+
+  set apellido(nuevoApellido) {
+    this.#apellido = nuevoApellido;
+  }
+
+  set telefono(nuevoTelefono) {
+    this.#telefono = nuevoTelefono;
+  }
+
+  set email(nuevoEmail) {
+    this.#email = nuevoEmail;
+  }
+
+  set imagen(nuevaImagen) {
+    this.#imagen = nuevaImagen;
+  }
+
+  set empresa(nuevaEmpresa) {
+    this.#empresa = nuevaEmpresa;
+  }
+
+  set puestoTrabajo(nuevoPuestoTrabajo) {
+    this.#puestoTrabajo = nuevoPuestoTrabajo;
+  }
+
+  set direccion(nuevaDireccion) {
+    this.#direccion = nuevaDireccion;
+  }
+
+  set notas(nuevasNotas) {
+    this.#notas = nuevasNotas;
+  }
 }
